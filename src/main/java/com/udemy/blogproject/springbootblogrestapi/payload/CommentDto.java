43eaebe0ef232db7e4c56008119e567069a4fe27
Hac90111 <chaudharihimanshu11@gmail.com>
2022-11-28 10:25:29 -0500

    Customized swagger docs with decription of model and controllers with Annotations

--- src/main/java/com/udemy/blogproject/springbootblogrestapi/payload/CommentDto.java
+++ src/main/java/com/udemy/blogproject/springbootblogrestapi/payload/CommentDto.java
@@ -1,24 +1,32 @@
 package com.udemy.blogproject.springbootblogrestapi.payload;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.springframework.format.annotation.NumberFormat;
 
 import javax.validation.constraints.*;
 
 @Data
+@ApiModel(description = "Comment model information")
 public class CommentDto {
+
+    @ApiModelProperty(value = "Blog comment id")
     private long id;
 
-    @NotEmpty(message = "Name cannnot be empty or null")
+    @NotEmpty(message = "Name cannot be empty or null")
     @Min(2)
     @Pattern(regexp="^[A-Za-z]*$",message = "Invalid Input")  // validates that it doesn't have numeric values
+    @ApiModelProperty(value = "Blog comment name")
     private String name;
 
     @NotEmpty(message = "Email is a mandatory field")
     @Email
+    @ApiModelProperty(value = "Blog comment email")
     private  String email;
 
-    @NotEmpty(message = "body cannot be empty or null")
-    @Size(min = 10,message = "minimum 10 chars required")
+    @NotEmpty(message = "Body cannot be empty or null")
+    @Size(min = 10,message = "Minimum 10 chars required")
+    @ApiModelProperty(value = "Blog comment body")
     private String body;
 }
--- src/main/java/com/udemy/blogproject/springbootblogrestapi/payload/PostDto.java
+++ src/main/java/com/udemy/blogproject/springbootblogrestapi/payload/PostDto.java
@@ -1,5 +1,7 @@
 package com.udemy.blogproject.springbootblogrestapi.payload;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
@@ -8,19 +10,26 @@ import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Data
+@ApiModel(description = "Post model information")
 public class PostDto {
+
+    @ApiModelProperty(value = "Blog post id")
     private long id;
     //title should not be empty or null, have at least 2 chars
     @NotEmpty
     @Size(min = 2, message = "Post title should have at least 2 characters")
     @Pattern(regexp="^[A-Za-z]*$",message = "Invalid Input")  // validates that it doesn't have numeric values
+    @ApiModelProperty(value = "Blog post title")
     private String title;
 
     @NotEmpty
     @Size(min = 10, message = "Post-description should have at least 10 characters")
+    @ApiModelProperty(value = "Blog post description")
     private String description;
 
     @NotEmpty
+    @ApiModelProperty(value = "Blog post content")
     private String content;
+    @ApiModelProperty(value = "Blog post comments")
     private Set<CommentDto> comments;
 }
